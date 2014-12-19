3974cfcca557d0c9f1c9859e440d28893990f466
Dan Heidel <dan.heidel@gmail.com>
2014-12-18 17:30:59 -0800

    working, sending SMS

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ var fs = require('fs');
 var async = require('async');
 var cheerio = require('cheerio');
 var request = require('request');
+var twilio = require('twilio');
 
 var configPath = process.argv[2] || 'config.json';
+var twilioPath = process.argv[3] || 'twilio.json';
 var queryArray = [];
+var twilioClient, twilioSID, twilioToken, twilioNumber;
 
 async.series([
+  loadTwilioData,
   loadConfig,
   initQueries
 ]);
+
+function loadTwilioData(callback){
+  fs.readFile(twilioPath, function(err, data){
+    if(err){
+      console.error('failed to read Twilio config file');
+      process.exit();
+    }
+    var parsedData = JSON.parse(data);
+    twilioSID = parsedData.sid;
+    twilioToken = parsedData.token;
+    twilioNumber = parsedData.number;
+    twilioClient = new twilio.RestClient(twilioSID, twilioToken);
+    callback();
+  });
+}
 
 function loadConfig(callback){
   var fileData;
@@ -26,12 +45,10 @@ function loadConfig(callback){
         interval: parsedData[rep].interval,
         errorInterval: parsedData[rep].errorInterval,
         email: parsedData[rep].email,
-        sms: parsedData[rep].sms,
-        startTime: new Date()
+        sms: parsedData[rep].sms
       };
       queryArray.push(tempPage);
     }
-
     callback();
   });
 }
@@ -46,14 +63,29 @@ function initQueries(callback){
 function queryResponse(query, err, resp){
   if(err || resp.statusCode != 200){
     handleError(query, err, resp);
+    setTimeout(request, query.errorInterval * 1000, query.url, queryResponse.bind(null, query));
+  } else {
+    console.log(query.name);
+    console.log(err);
+    console.log((resp ? resp.statusCode : 'no response'));
+    setTimeout(request, query.interval * 1000, query.url, queryResponse.bind(null, query));
   }
-
-  console.log(query);
-  console.log(err);
-  console.log((resp ? resp.statusCode : 'no response'));
-
 }
 
 function handleError(query, err, resp){
   console.log('error: ' + err);
+  twilioClient.sms.messages.create({
+    to: query.sms,
+    from: twilioNumber,
+    body: 'error accessing: ' + query.url
+  }, function(err, reply){
+    if(err){
+      console.error('there was an error');
+      console.log(err);
+    } else {
+      console.log('sent message, sid: ' + reply.sid);
+      console.log('message sent to: ' + query.sms);
+      console.log('message sent: ' + reply.dateCreated);
+    }
+  })
 }
